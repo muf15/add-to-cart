d36c7d26c25f3f341d9ef1dec477d1a6897d4ccd
Mufaddal <mufaddaltopiwala15@gmail.com>
2024-06-27 07:25:28 +0530

    removed comments

--- index.js
+++ index.js
@@ -14,13 +14,9 @@ const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 const emptyMessageEl = document.getElementById("empty-message")
 
-// Execute a function when the user presses a key on the keyboard
 inputFieldEl.addEventListener("keypress", function (event) {
-    // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter") {
-        // Cancel the default action, if needed
         event.preventDefault();
-        // Trigger the button element with a click
         document.getElementById("add-button").click();
     }
 });
